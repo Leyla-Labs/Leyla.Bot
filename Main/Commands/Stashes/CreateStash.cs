f3e34107638580927d770740ad55a7841a0832bb
Tawmy <mail@tawmy.dev>
2022-05-12 07:47:25 +0200

    Add CreateStash command

---
+++ Main/Commands/Stashes/CreateStash.cs
@@ -0,0 +1,51 @@
+using Common.Classes;
+using Db;
+using Db.Models;
+using DSharpPlus;
+using DSharpPlus.Entities;
+using DSharpPlus.SlashCommands;
+
+namespace Main.Commands.Stashes;
+
+public class CreateStash : SlashCommand
+{
+    private readonly string _stashName;
+    
+    public CreateStash(InteractionContext ctx, string stashName) : base(ctx)
+    {
+        _stashName = stashName;
+    }
+
+    public override async Task RunAsync()
+    {
+        await CreateStashInDatabase(null);
+        await Ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource,
+            new DiscordInteractionResponseBuilder().AddEmbed(GetConfirmationEmbed()).AsEphemeral());
+    }
+
+    #region Instance methods
+    
+    private async Task CreateStashInDatabase(ulong? requiredRoleId)
+    {
+        await using var context = new DatabaseContext();
+        await context.Stashes.AddAsync(new Stash
+        {
+            Name = _stashName,
+            GuildId = Ctx.Guild.Id,
+            RequiredRoleId = requiredRoleId
+        });
+        await context.SaveChangesAsync();
+    }
+
+    private DiscordEmbed GetConfirmationEmbed()
+    {
+        var embed = new DiscordEmbedBuilder();
+        embed.WithTitle("Stash Created");
+        embed.WithDescription(
+            $"The stash `{_stashName}` has been created. You can right click messages to add entries to it.");
+        embed.WithColor(DiscordColor.Blurple);
+        return embed.Build();
+    }
+    
+    #endregion
+}
--- Main/Modules/Stashes.cs
+++ Main/Modules/Stashes.cs
@@ -1,14 +1,25 @@
 using DSharpPlus;
 using DSharpPlus.SlashCommands;
+using DSharpPlus.SlashCommands.Attributes;
 using Main.Commands.Stashes;
 
 namespace Main.Modules;
 
+[SlashCommandGroup("Stash", "Description TODO")]
+[SlashRequireGuild]
 public class Stashes : ApplicationCommandModule
 {
     [ContextMenu(ApplicationCommandType.MessageContextMenu, "Add to Stash")]
     public async Task MenuAddToStash(ContextMenuContext ctx)
     {
         await new AddToStash(ctx).RunAsync();
+    }
+
+    [SlashCommand("create", "Create a new stash.")]
+    public async Task SlashCreateStash(InteractionContext ctx,
+        [Option("Name", "Name of the stash to create")]
+        string title)
+    {
+        await new CreateStash(ctx, title).RunAsync();
     }
 }
