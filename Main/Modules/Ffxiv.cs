6c282265829c72e1a06f71e34900653341bcec7f
Tawmy <mail@tawmy.dev>
2022-10-25 15:08:16 +0200

    Add empty FFXIV module

---
+++ Main/Modules/Ffxiv.cs
@@ -0,0 +1,11 @@
+using Common.Classes;
+using DSharpPlus.SlashCommands;
+using DSharpPlus.SlashCommands.Attributes;
+
+namespace Main.Modules;
+
+[SlashCommandGroup("FFXIV", "Description TODO")]
+[SlashRequireGuild]
+public class Ffxiv : ApplicationCommandLogModule
+{
+}
